a20ae7b6ea08e64a4dfa476c31b35b5bdf5f7e73
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-03-29 22:04:46 -0500

    created functions for generating questions and starting the quiz

--- assets/script.js
+++ assets/script.js
@@ -49,13 +49,88 @@ var quizQuestions = [{
 
 ]
 
-// create a function to begin the quiz on the start button
+// create more global variables for question index and timer
 
-function showQuiz(){
+var questionIndex = 0;
+var finalQuestionIndex = quizQuestions.length
+var timeLeft = 60
+var timerInterval;
+var score = 0;
+var correct;
 
+// create a function to show questions during the quiz, and hide them on the final screen
+
+function generateQuestion(){
+    gameoverDiv.style.display = "none";
+    if (currentQuestionIndex === finalQuestionIndex){
+        return showScore();
+    } 
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+    questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
+    buttonA.innerHTML = currentQuestion.choiceA;
+    buttonB.innerHTML = currentQuestion.choiceB;
+    buttonC.innerHTML = currentQuestion.choiceC;
+    buttonD.innerHTML = currentQuestion.choiceD;
+
+
+// create a function to begin the quiz and timer on the start button
+
+function startQuiz(){
+    gameoverDiv.style.display = "none";
+    startQuizDiv.style.display = "none";
+    generateQuestion();
+
+    //create timer functionality
+
+    timerInterval = setInterval(function() {
+        timeLeft--;
+        timer.textContent = "Time left: " + timeLeft;
+    
+        if(timeLeft === 0) {
+          clearInterval(timerInterval);
+          showScore();
+        }
+      }, 1000);
+    quizContainer.style.display = "block";
 }
+
+// create function to display score after quiz is done or no time left
+
+function displayScore(){
+    quizContainer.style.display = "none"
+    gameOver.style.display = "flex";
+    clearInterval(timerInterval);
+    initials.value = "";
+    finalScore.innerHTML = "You got " + score + " out of " + quizQuestions.length + " correct!";
+}
+
+// on submit, create a function that saves in local storage and pushes the new initials and score into the array
+submitScoreBtn.addEventListener("click", function allScores){
+    if(initials.value === "") {
+        alert("Please input a value for your initials");
+        return false;
+    }else{
+        var savedScores = JSON.parse(localStorage.getItem("savedScores")) || [];
+        var userName = initials.value.trim();
+        var topScore = {
+            name : userName,
+            score : score
+        };
+
+        gameOver.style.display = "none";
+        scoreContainer.style.display = "flex";
+        scorePage.style.display = "block";
+        
+        savedScores.push(topScore);
+        localStorage.setItem("savedScores", JSON.stringify(topScores));
+        generateScore();
+    }
+}
+
+// create a function to check answers as you go
+
+
 
 // call quiz to display first
 
-showQuiz();
-
+startBtn.addEventListener("click", startQuiz);
