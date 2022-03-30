3bb9842461ed1f47c3910778838c535196850cc8
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-03-29 19:36:45 -0500

    adjusted various names in HTML for clarity in JS, added corresponding variables to JS

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,32 @@
 // create variables for the question container, score, and start button
-
+// quiz home and start button
 var quizContainer = document.getElementById('quiz');
-var score = document.getElementById('score');
 var timer = document.getElementById('timer');
+var startScrn = document.getElementById("startScreen");
 var startBtn = document.getElementById('startBtn');
 
-// create an array to house questions and options
+// questions and answer buttons
+var questionsEl = document.getElementById("questions");
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
 
+// game over screen and initials/high score display
+var gameOver = document.getElementById("gameOver");
+
+var submitScoreBtn = document.getElementById("submitScore");
+var scoreContainer = document.getElementById("scoreContainer");
+var scorePage = document.getElementById("scorePage");
+var initials = document.getElementById("initials");
+var scoreEl = document.getElementById("score");
+var userInitials = document.getElementById("userInitials");
+var finalScore = document.getElementById("finalScore");
+
+// create an array to house questions and options
+var quizQuestions = [{
+    
+}]
 
 // create a function to begin the quiz on the start button
 
